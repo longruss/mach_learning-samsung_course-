0f9e8b9335f60b56eb846e43a6a145178394d355
longruss <43468727+longruss@users.noreply.github.com>
2020-05-28 08:22:32 +0300

    Create 2_3(with optimizer).py

---
+++ 2_3(with optimizer).py
@@ -0,0 +1,11 @@
+import torch
+
+w = torch.tensor([[5., 10.], [1., 2.]], requires_grad=True)
+alpha = 0.001
+
+for _ in range(500):
+    # it's critical to calculate function inside the loop:
+    function = (w + 7).log().log().prod()
+    function.backward()
+    w.data -= alpha * w.grad  # put our code here
+    w.grad.zero_()
